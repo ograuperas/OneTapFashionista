61911221f4ff853569505c78940a32d655cb070d
guillem-ms <guillemmsbi@gmail.com>
2021-05-23 10:54:53 +0200

    predict v1

--- Code/training_model.py
+++ Code/training_model.py
@@ -1,43 +1,114 @@
-import cv2
-import numpy as np
-import matplotlib.pyplot as plt
-import git
-import sys
 import os
-import subprocess
+import torch
+import numpy as np
+from PIL import Image
+from tqdm import tqdm
+
+from torch.utils.data import DataLoader
+import torchvision.transforms as transforms
+
+import networks
+from utils.transforms import transform_logits
+from datasets.simple_extractor_dataset import SimpleFolderDataset
 
 
-def dowload_model():
-        
-    if os.path.exists('Self-Correction-Human-Parsing') == False:
-        subprocess.call('git clone https://github.com/PeikeLi/Self-Correction-Human-Parsing')
-    os.chdir('Self-Correction-Human-Parsing')
-    if os.path.exists('checkpoint') == False:
-        os.mkdir('checkpoint')
-        os.mkdir('inputs')
-        os.mkdir('outputs')
-    
-    dataset = 'lip'         #select from ['lip', 'atr', 'pascal']
-    import gdown
-    
-    if dataset == 'lip':
-        url = 'https://drive.google.com/uc?id=1k4dllHpu0bdx38J7H28rVVLpU-kOHmnH'
-    elif dataset == 'atr':
-        url = 'https://drive.google.com/uc?id=1ruJg4lqR_jgQPj-9K0PP-L2vJERYOxLP'
-    elif dataset == 'pascal':
-        url = 'https://drive.google.com/uc?id=1E5YwNKW2VOEayK9mWCS3Kpsxf-3z04ZE'
-    
-    output = 'checkpoint/final.pth'
-    if os.path.exists(output) == False:
-        gdown.download(url, output, quiet=False)
-    
-    
+dataset_settings = {
+    'lip': {
+        'input_size': [473, 473],
+        'num_classes': 20,
+        'label': ['Background', 'Hat', 'Hair', 'Glove', 'Sunglasses', 'Upper-clothes', 'Dress', 'Coat',
+                  'Socks', 'Pants', 'Jumpsuits', 'Scarf', 'Skirt', 'Face', 'Left-arm', 'Right-arm',
+                  'Left-leg', 'Right-leg', 'Left-shoe', 'Right-shoe']
+    }
+}
 
-def make_prediction():
-    os.chdir('inputs')
-    #penjar imatge a input
-    
-    os.chdir('..')
-    
-    main2()
-    #("python3 simple_extractor.py --dataset 'lip' --model-restore 'checkpoints/final.pth' --input-dir 'inputs' --output-dir 'outputs' ")
+def get_palette(num_cls):
+    """ Returns the color map for visualizing the segmentation mask.
+    Args:
+        num_cls: Number of classes
+    Returns:
+        The color map
+    """
+    n = num_cls
+    palette = [0] * (n * 3)
+    for j in range(0, n):
+        lab = j
+        palette[j * 3 + 0] = 0
+        palette[j * 3 + 1] = 0
+        palette[j * 3 + 2] = 0
+        i = 0
+        while lab:
+            palette[j * 3 + 0] |= (((lab >> 0) & 1) << (7 - i))
+            palette[j * 3 + 1] |= (((lab >> 1) & 1) << (7 - i))
+            palette[j * 3 + 2] |= (((lab >> 2) & 1) << (7 - i))
+            i += 1
+            lab >>= 3
+    return palette
+
+def predict(model_path, input_dir, output_dir)
+
+
+    os.environ["CUDA_VISIBLE_DEVICES"] = args.gpu
+
+    """
+    Lo de encima va fuera, solo queda substituir el código por algo que haga un set sobre la GPU.
+    """
+    num_classes = dataset_settings['lip']['num_classes']
+    input_size = dataset_settings['lip']['input_size']
+    label = dataset_settings['lip']['label']
+    print("Evaluating total class number {} with {}".format(num_classes, label))
+
+    model = networks.init_model('resnet101', num_classes=num_classes, pretrained=None)
+
+    state_dict = torch.load(model_path)['state_dict']
+    from collections import OrderedDict
+    new_state_dict = OrderedDict()
+    for k, v in state_dict.items():
+        name = k[7:]  # remove `module.`
+        new_state_dict[name] = v
+    model.load_state_dict(new_state_dict)
+    model.cuda()
+    model.eval()
+
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.406, 0.456, 0.485], std=[0.225, 0.224, 0.229])
+    ])
+    dataset = SimpleFolderDataset(root=input_dir, input_size=input_size, transform=transform)
+    dataloader = DataLoader(dataset)
+
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    palette = get_palette(num_classes)
+    with torch.no_grad():
+        for idx, batch in enumerate(tqdm(dataloader)):
+            image, meta = batch
+            img_name = meta['name'][0]
+            c = meta['center'].numpy()[0]
+            s = meta['scale'].numpy()[0]
+            w = meta['width'].numpy()[0]
+            h = meta['height'].numpy()[0]
+
+            output = model(image.cuda())
+            upsample = torch.nn.Upsample(size=input_size, mode='bilinear', align_corners=True)
+            upsample_output = upsample(output[0][-1][0].unsqueeze(0))
+            upsample_output = upsample_output.squeeze()
+            upsample_output = upsample_output.permute(1, 2, 0)  # CHW -> HWC
+
+            logits_result = transform_logits(upsample_output.data.cpu().numpy(), c, s, w, h, input_size=input_size)
+            parsing_result = np.argmax(logits_result, axis=2)
+            parsing_result_path = os.path.join(output_dir, img_name[:-4] + '.png')
+            output_img = Image.fromarray(np.asarray(parsing_result, dtype=np.uint8))
+            output_img.putpalette(palette)
+            output_img.save(parsing_result_path)
+
+    return
+
+
+
+
+
+
+if __name__ == "__main__":
+    predict('checkpoints/final.pth', 'inputs', 'outputs')
