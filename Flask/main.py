43c51b506b81526f954f9cf7571da22abc8ff0e2
Oriol Graupera <49688038+OriGraupera@users.noreply.github.com>
2021-05-26 17:22:05 +0200

    Update main.py

--- Flask/main.py
+++ Flask/main.py
@@ -32,16 +32,7 @@ def getImage():
     image = bytes(resposta)
     nparr = np.frombuffer(image, np.uint8)
     im_input = cv2.imdecode(nparr, 1)
-    
-    client = storage.Client()
-    bucket = client.get_bucket('onetapfashionista.appspot.com')
-    print(type(bucket))
-    blob = bucket.blob('/out')
-    print(type(blob))
-    blob.download_to_filename('/workspace/img/out')
-    
-    
-    
+
     im_output,input_image,output_image = utils.return_mask(im_input)
 
     #cv2.imwrite('O:/Escriptori/SM/Flask/img/actuals/in1.jpg', im_input)
